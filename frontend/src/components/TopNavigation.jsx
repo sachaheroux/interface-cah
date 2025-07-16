e7a256df895025ac4dd9d7def74d394347ea82fc
Sacha Héroux <sacha.heroux87@gmail.com>
2025-07-15 23:08:25 -0400

    SVP

--- frontend/src/components/TopNavigation.jsx
+++ frontend/src/components/TopNavigation.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
 import { Link, useLocation } from 'react-router-dom'
 import { 
-  LayoutDashboard, 
   Building2, 
   Users, 
-  Wrench, 
   Receipt, 
   UserCheck, 
   Truck, 
