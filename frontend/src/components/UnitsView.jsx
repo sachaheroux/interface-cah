b9da0c3a9d567ba609a00dea15e90de9514fada5
Sacha Héroux <sacha.heroux87@gmail.com>
2025-07-18 20:50:15 -0400

    SVP

--- frontend/src/components/UnitsView.jsx
+++ frontend/src/components/UnitsView.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import { 
   Users, MapPin, Eye, Edit3, Trash2, Search, Filter, Home, Mail, Phone, DollarSign, 
-  Building2, Bed, Bath, Car, Wifi, Wind, CheckCircle, Clock, AlertCircle, UserMinus
+  Building2, Bed, Bath, Car, Wifi, Wind, CheckCircle, Clock, AlertCircle, UserMinus,
+  Thermometer, Zap, Sofa, Droplets, Package
 } from 'lucide-react'
 import { parseAddressAndGenerateUnits } from '../types/unit'
 import { calculateUnitStatus, getUnitStatusLabel, getUnitStatusColor, getUnitTypeLabel } from '../types/unit'
